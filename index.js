242eed493d72feeacea8030be6a8f3d6ba68b959
Guilherme dos Santos Jardim <132668598+GuiJardim69@users.noreply.github.com>
2023-09-01 11:15:05 +0000

    Adicionando

--- index.js
+++ index.js
@@ -45,13 +45,26 @@ app.get("/usuario/:id", async (req, res) => {
 //index.js
 app.use(express.json());
 //index.js
-import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
-//index.js
 app.post("/usuario", async (req, res) => {
   console.log("Rota POST /usuario solicitada");
   try {
     await insertUsuario(req.body);
     res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+//index.js
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario } from "./bd.js";
+//index.js
+app.delete("/usuario/:id", async (req, res) => {
+  console.log("Rota DELETE /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) {
+      await deleteUsuario(req.params.id);
+      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
