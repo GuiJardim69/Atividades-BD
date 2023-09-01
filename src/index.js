46c10305a2a7f98966836814f63fc18489310a69
Guilherme dos Santos Jardim <132668598+GuiJardim69@users.noreply.github.com>
2023-09-01 19:16:00 +0000

    adicinando 2.0

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(roteadorUsuario);
 
 app.get("/", (req, res) => {
   res.json({
@@ -18,4 +19,3 @@ app.listen(port, () => {
   // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
-app.use(roteadorUsuario);
