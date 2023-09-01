fedfc94c1f09fb7e577dc19a0c3e2fd21325318a
Guilherme dos Santos Jardim <132668598+GuiJardim69@users.noreply.github.com>
2023-09-01 10:58:37 +0000

    Adicionando

--- bd.js
+++ bd.js
@@ -24,4 +24,11 @@ async function selectUsuario(id) {
   return res.rows;
 }
 //bd.js
-export { selectUsuarios, selectUsuario };
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario };
--- index.js
+++ index.js
@@ -32,14 +32,26 @@ app.listen(port, () => {
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 //index.js
-import { selectUsuarios, selectUsuario } from "./bd.js";
-//index.js
 app.get("/usuario/:id", async (req, res) => {
   console.log("Rota GET /usuario solicitada");
   try {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) res.json(usuario);
     else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+//index.js
+app.use(express.json());
+//index.js
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
+//index.js
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
